3c0d574fd53a6816f4bc0cbae5b708f4b8bbdb07
Reugito <raosahebdhotreedu@gmail.com>
2021-08-25 12:40:23 +0530

    Validating the user name: - Name starts with Cap and has minimum 3 characters

--- src/main/java/com/blz/servlet/LoginServlet.java
+++ src/main/java/com/blz/servlet/LoginServlet.java
@@ -7,6 +7,8 @@ package com.blz.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
@@ -29,23 +31,24 @@ public class LoginServlet extends HttpServlet {
 		
                 String user = request.getParameter("user");
 		String psw = request.getParameter("psw");
+               
+                PrintWriter out = response.getWriter();
+                 boolean validateUser = Pattern.matches("[A-Z][a-z]{2,}",user);
                 
-                String userID = "Rao";
-		String password = "1212";
-	
-		if(userID.equals(user) && psw.equals(password)) {
+		if(validateUser) {
 			request.setAttribute("user", user);
 			request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+                       
 		}else {
 			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-			PrintWriter out = response.getWriter();
+			//PrintWriter out = response.getWriter();
 			out.println("<font color=red>Either user name or password is wrong</font>");
 			rd.include(request, response);
 		}
                 
-                PrintWriter out = response.getWriter();
+                 out = response.getWriter();
 		out.println("<h1>This is my first servlet page</h1>");
 		out.close();
-	}
+	} 
 
 }
