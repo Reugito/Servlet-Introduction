77161fa17715bd15294964f04233aabd9317cec8
Reugito <raosahebdhotreedu@gmail.com>
2021-08-25 12:06:55 +0530

    My first servlet project

---
+++ src/main/java/com/blz/servlet/LoginServlet.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blz.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+		//urlPatterns = {"/LoginServlet"},
+		initParams = {
+				@WebInitParam( name= "user", value = "Rao"),
+				@WebInitParam( name= "password", value = "123")		
+		}
+)
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+                String user = request.getParameter("user");
+		String psw = request.getParameter("psw");
+                
+                String userID = "Rao";
+		String password = "1212";
+	
+		if(userID.equals(user) && psw.equals(password)) {
+			request.setAttribute("user", user);
+			request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+		}else {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+			PrintWriter out = response.getWriter();
+			out.println("<font color=red>Either user name or password is wrong</font>");
+			rd.include(request, response);
+		}
+                
+                PrintWriter out = response.getWriter();
+		out.println("<h1>This is my first servlet page</h1>");
+		out.close();
+	}
+
+}
